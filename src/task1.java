015a9a4f142a36ba9dc0ec06dc7ecf8aaf772090
IslaBecker91031015 <islabecker@gmail.com>
2018-10-15 12:39:56 +1300

    Update src/task1.java

---
+++ src/task1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class task1 {
+    public static void main(String[] args)
+    {
+        String[] grades = {"A", "B", "C", "D", "F"};
+
+        System.out.println(Arrays.toString(grades));
+    }
+
+}
