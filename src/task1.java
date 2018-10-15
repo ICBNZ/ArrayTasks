7e10a8fc625e5465951a9e569c7ff7004c4c94f6
IslaBecker91031015 <islabecker@gmail.com>
2018-10-15 15:56:35 +1300

    task 1 & 2

--- src/task1.java
+++ src/task1.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 public class task1 {
     public static void main(String[] args) {
+        //Task 1
         String[] grades = new String[5];
         grades[0] ="A";
         grades[1] ="B";
@@ -11,15 +12,34 @@ public class task1 {
         // new value
         String newGrade = "C";
         // print old
-        System.out.println("Old Array: "+Arrays.toString(grades));
+        System.out.println("Old Grades Array: "+Arrays.toString(grades));
 
         for (int i = grades.length-1; i > pos; i--) {
             grades[i] = grades[i-1];
         }
         grades[pos] = newGrade;
         // print new
-        System.out.println("New Array: "+Arrays.toString(grades));
-        }
+        System.out.println("New Grades Array: "+Arrays.toString(grades));
+
+        //Task 2
+        String[] subjects = {"HTML", "CSS", "Java"};
+
+        // print second
+        System.out.println("2nd Subject: "+(subjects[1]));
+
+        // new subjects
+        String new1 = "Javascript";
+        String new2 = "Python";
+
+        String[] newA = Arrays.copyOf(subjects, 5);
+        newA[newA.length-1] = new1;
+        newA[newA.length-2] = new2;
+
+        // print all
+        System.out.println("All Subjects: "+Arrays.toString(newA));
+
+
+    }
 
     }
 
