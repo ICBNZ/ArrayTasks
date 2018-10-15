65f6741c5acb86f6696b539f9015c54e6fa8b4b6
IslaBecker91031015 <islabecker@gmail.com>
2018-10-15 15:13:02 +1300

    initial array

--- src/task1.java
+++ src/task1.java
@@ -1,10 +1,25 @@
 import java.util.Arrays;
 public class task1 {
-    public static void main(String[] args)
-    {
-        String[] grades = {"A", "B", "D", "F"};
+    public static void main(String[] args) {
+        String[] grades = new String[5];
+        grades[0] ="A";
+        grades[1] ="B";
+        grades[2] ="D";
+        grades[3] ="F";
+        //index
+        int pos = 2;
+        // new value
+        String newGrade = "C";
+        // print old
+        System.out.println("Old Array: "+Arrays.toString(grades));
 
-        System.out.println(Arrays.toString(grades));
+        for (int i = grades.length-1; i > pos; i--) {
+            grades[i] = grades[i-1];
+        }
+        grades[pos] = newGrade;
+        // print new
+        System.out.println("New Array: "+Arrays.toString(grades));
+        }
+
     }
 
-}
